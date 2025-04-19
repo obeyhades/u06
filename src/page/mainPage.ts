e2472f48f0b6e4902d297fc4e42a0b85047ad704
obeyhades <abdu.nazar145@gmail.com>
2025-04-19 15:02:33 +0200

    added mainpage

---
+++ src/page/mainPage.ts
@@ -0,0 +1,22 @@
+import "../style.css"
+
+const totalAmount = document.getElementById("totalAmount") as HTMLElement;
+const addIncomeBtn = document.getElementById("addIncomeBtn");
+
+let income = Number(localStorage.getItem("income")) || 0;
+
+
+totalAmount.textContent = `$${income}`;
+
+addIncomeBtn?.addEventListener("click", () => {
+  const newIncomeStr = prompt("Enter your monthly income:");
+  const newIncome = Number(newIncomeStr);
+
+  if (!isNaN(newIncome)) {
+    income = newIncome;
+    localStorage.setItem("income", income.toString());
+    totalAmount.textContent = `$${income}`;
+  } else {
+    alert("Please enter a valid number.");
+  }
+});
