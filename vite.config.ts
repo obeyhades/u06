1a841c0e4a56524beb465e1d0427501fea6c0f57
obeyhades <abdu.nazar145@gmail.com>
2025-04-20 21:28:54 +0200

    work?

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,15 @@
 import { dirname, resolve } from 'node:path';
 import { fileURLToPath } from 'node:url';
 import { defineConfig } from 'vite';
+import tailwindcss from '@tailwindcss/vite'
+
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
+  plugins: [
+    tailwindcss(),
+  ],
   build: {
     rollupOptions: {
       input: {
@@ -17,3 +22,5 @@ export default defineConfig({
     },
   },
 });
+
+
