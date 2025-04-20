e26d9d5eb968d7e8c77d4e31a9bb1e61e1972640
obeyhades <abdu.nazar145@gmail.com>
2025-04-20 21:21:12 +0200

    deploy plz

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
+import { dirname, resolve } from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { defineConfig } from 'vite';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 export default defineConfig({
-  plugins: [
-    tailwindcss(),
-  ],
-})
+  build: {
+    rollupOptions: {
+      input: {
+        index: resolve(__dirname, 'index.html'),
+        editprofile: resolve(__dirname, 'editProfile.html'),
+        login: resolve(__dirname, 'login.html'),
+        mainpage: resolve(__dirname, 'mainPage.html'),
+        register: resolve(__dirname, 'register.html'),
+      },
+    },
+  },
+});
